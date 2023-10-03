9f8751a07767edb9f5d3ca44f7db47e7ac7682f1
milliarderlzt <146845848+milliarderlzt@users.noreply.github.com>
2023-10-03 16:18:35 +0300

    l
    l

---
+++ config.js
@@ -0,0 +1,16 @@
+//  УСТАНОВКА ССЫЛОК
+data = {
+  lolz: 'https://zelenka.guru/milliarder/',
+  telegram: 'https://t.me/anchixa',
+};
+//  УСТАНОВКА ССЫЛОК
+
+
+
+
+//  НЕ ТРОГАТЬ
+const lolzButton = document.querySelector('#lolz');
+const telegramButton = document.querySelector('#telegram');
+lolzButton.setAttribute('href', data.lolz);
+telegramButton.setAttribute('href', data.telegram);
+//  НЕ ТРОГАТЬ
